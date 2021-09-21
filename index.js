d330b2ba74094130d806a94661c3fcd714edbcdb
olivia <ojeglum@gmail.com>
2021-09-20 20:25:01 -0700

    Completed assignment

--- index.js
+++ index.js
@@ -1,11 +1,35 @@
-function scuberGreetingForFeet(){
-  // Write your code here!
+function scuberGreetingForFeet(distance){
+  let result
+  if (distance <= 400) {
+    result = 'This one is on me!';
+  } else if (distance > 2000 && distance < 2500) {
+    result = 'I will gladly take your thirty bucks.';
+  } else if (distance > 2500) {
+    result = 'No can do.';
+  } 
+  
+  return result;
 }
 
-function ternaryCheckCity(){
-  // Write your code here!
+function ternaryCheckCity(city){
+  let result
+  city === 'NYC' ? (result = 'Ok, sounds good.') : (result = 'No go.');
+  return result;
 }
 
-function switchOnCharmFromTip(){
-  // Write your code here!
+function switchOnCharmFromTip(tip){
+  const amount = 'generous';
+  let result;
+  switch(tip) {
+    case 'generous':
+      result = 'Thank you so much.';
+      break;
+    case 'not as generous':
+      result = 'Thank you.';
+      break;
+    case 'thanks for everything':
+      result = 'Bye.';
+      break;
+  }
+  return result;
 }
